4f1c62ccd7967eaeb3e6dcede7615feb34cbf785
RenanCruz7 <renancruz199@gmail.com>
2025-03-27 22:45:57 -0300

    for and if else tests

--- for.go
+++ for.go
@@ -7,4 +7,27 @@ func main() {
 	for i := 1; i <= 10; i++ {
 		fmt.Println(i)
 	}
+
+	j := 0
+	for j <= 3 {
+		fmt.Println(j)
+		j++
+	}
+
+	for k := range 5 {
+		fmt.Println(k)
+	}
+
+	for {
+		fmt.Println("loop")
+		break
+	}
+
+	for n := range 6 {
+		if n%2 == 0 {
+			continue
+		}
+		fmt.Println(n)
+	}
+
 }
---
+++ if-else.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func main() {
+	// If-else statement
+	a := 10
+	b := 20
+
+	if a > b {
+		fmt.Println("A is greater than B")
+	} else if a < b {
+		fmt.Println("A is less than B")
+	} else {
+		fmt.Println("A is equal to B")
+	}
+
+}
